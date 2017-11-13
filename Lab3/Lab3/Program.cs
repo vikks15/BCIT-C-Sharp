6c71ca947004fcbeaf35dfcf12385f5c28ef51da
vikks15 <lva9@mail.ru>
2017-11-14 01:37:21 +0300

    matrix (x,y,z), bad print

--- Lab3/Lab3/Program.cs
+++ Lab3/Lab3/Program.cs
@@ -5,7 +5,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
- 
+
 namespace Lab3
 {
     class FigureMatrCheckEmpty
@@ -31,39 +31,43 @@ namespace Lab3
         Dictionary<string, T> _matrix = new Dictionary<string, T>();
         int maxX; // max number of columns
         int maxY; // max number of strings
+        int maxZ;
+        string name;
 
         T nullElement; // Empty element, returns if there is no element with needed coordinats 
-        public Matrix(int px, int py, T nullElementParam) //Constructor
+        public Matrix(string name, int px, int py, int pz, T nullElementParam) //Constructor
         {
             this.maxX = px;
             this.maxY = py;
+            this.maxZ = pz;
+            this.name = name;
             this.nullElement = nullElementParam;
         }
 
-        void CheckBounds(int x, int y)
+        void CheckBounds(int x, int y, int z)
         {
-            if (x < 0 || x >= this.maxX) throw new Exception("x=" + x + " overflow");
-
-            if (y < 0 || y >= this.maxY) throw new Exception("y=" + y + " overflow");
+            if (x < 0 || x >= this.maxX) throw new Exception("x=" + x + " out of bounds");
+            if (y < 0 || y >= this.maxY) throw new Exception("y=" + y + " out of bounds");
+            if (z < 0 || z >= this.maxZ) throw new Exception("z=" + z + " out of bounds");
         }
         /// <summary>
         /// Form the key
         /// </summary>
-        string DictKey(int x, int y)
+        string DictKey(int x, int y, int z)
         {
-            return x.ToString() + "_" + y.ToString();
+            return x.ToString() + "_" + y.ToString() + "_" + z.ToString();
         }
 
         /// <summary>
         /// Indexer to access data
         /// </summary>
-        public T this[int x, int y]
+        public T this[int x, int y, int z]
         {
 
             get
             {
-                CheckBounds(x, y);
-                string key = DictKey(x, y);
+                CheckBounds(x, y, z);
+                string key = DictKey(x, y, z);
                 if (this._matrix.ContainsKey(key))
                 {
                     return this._matrix[key];
@@ -75,8 +79,8 @@ namespace Lab3
             }
             set
             {
-                CheckBounds(x, y);
-                string key = DictKey(x, y);
+                CheckBounds(x, y, z);
+                string key = DictKey(x, y, z);
                 this._matrix.Add(key, value);
             }
         }
@@ -85,16 +89,20 @@ namespace Lab3
         {
             //Class StringBuilder - for long strings, faster then making from lots of normal str   
             StringBuilder b = new StringBuilder();
-            for (int j = 0; j < this.maxY; j++)
+            for (int i = 0; i < this.maxZ; i++)
             {
-                b.Append("[");
-                for (int i = 0; i < this.maxX; i++)
+                for (int j = 0; j < this.maxY; j++)
                 {
-                    if (i > 0) b.Append("\t");
-                    if (this[i, j].Equals(nullElement)) b.Append("-");
-                    else b.Append(this[i, j].ToString());
+                
+                    for (int z = 0; z < this.maxX; z++)
+                    {
+                        if (z > 0) b.Append("\t");
+                        b.Append(this.name + "[" + i + "," + j + "," + z + "]: ");
+                        if (this[i, j, z].Equals(nullElement)) b.Append("  -  ");
+                        else b.Append(this[i, j, z].ToString());
+                    }
+                    b.Append("\n");
                 }
-                b.Append("]\n");
             }
             return b.ToString();
         }
@@ -191,7 +199,7 @@ namespace Lab3
 
     class Program
     {
-        
+
         static void Main(string[] args)
         {
             double a = 3, b = 4, r = 5;
@@ -205,7 +213,7 @@ namespace Lab3
             al.Add(circ);
             al.Sort();
             Console.WriteLine("ArrayList");
-            foreach(object obj in al) Console.WriteLine(obj.ToString()); //or through str type = obj.GetType().Name + ifs
+            foreach (object obj in al) Console.WriteLine(obj.ToString()); //or through str type = obj.GetType().Name + ifs
 
             Rectangle rec2 = new Rectangle(4, 5);
             Square sq2 = new Square(4);
@@ -223,14 +231,14 @@ namespace Lab3
 
             Console.WriteLine("\nMatrix");
             Square nullFigure = new Square(0);
-            Matrix<GeomFigure> matr = new Matrix<GeomFigure>(3, 3, nullFigure);
-            matr[0, 0] = rec;
-            matr[1, 1] = sq;
-            matr[2, 2] = circ;
+            Matrix<GeomFigure> matr = new Matrix<GeomFigure>("matr", 2, 2, 2, nullFigure);
+            matr[0, 0, 0] = rec;
+            matr[1, 1, 1] = sq;
+            // matr[2, 2, 2] = circ;
             Console.WriteLine(matr.ToString());
             Console.ReadKey();
 
         }
-       
+
     }
 }
