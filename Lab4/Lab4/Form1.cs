e12d436291fc81312cf2f8777540d3110be3cdbc
vikks15 <lva9@mail.ru>
2017-12-23 00:55:32 +0300

    optimisation

--- Lab4/Lab4/Form1.cs
+++ Lab4/Lab4/Form1.cs
@@ -25,19 +25,24 @@ namespace Lab4
         List<string> list = new List<string>();
         
         private void choosefile_Click(object sender, EventArgs e)
-        { 
+        {
             OpenFileDialog fileDialog= new OpenFileDialog();
             fileDialog.Filter = "текстовые файлы|*.txt";
-           
 
             if (fileDialog.ShowDialog() == DialogResult.OK)
             {
+                listBox.Items.Clear();
+                listBoxResults.Items.Clear();
+                Timer2Box.Clear();
+                EnterWordBox.Clear();              
                 Stopwatch timer = new Stopwatch();
                 timer.Start();
 
                 string text = File.ReadAllText(fileDialog.FileName); //read text from file in string
                 char[] separators = new char[] { ' ', '.', ',', '!', '?', '/', '\t', '\n'};
                 string[] textArray = text.Split(separators);
+
+                listBox.BeginUpdate();
                 foreach (string tempStr in textArray)
                 {
                     string str = tempStr.Trim(); //delete spaces
@@ -46,12 +51,12 @@ namespace Lab4
                         list.Add(str); //add word to list
                         //TextBox.Text += str + "\r\n";
                         listBox.Items.Add(str);
-                    }
-                                                           
+                    }                                     
                 }
+                listBox.EndUpdate();   
                 timer.Stop();
-                this.Timer1Box.Text = timer.Elapsed.ToString();
-                this.WordsCountBox.Text = list.Count.ToString();
+                Timer1Box.Text = timer.Elapsed.ToString();
+                WordsCountBox.Text = list.Count.ToString();
             }
             else MessageBox.Show("Choose file!");
         }
@@ -59,27 +64,25 @@ namespace Lab4
         private void SearchWordButton_Click(object sender, EventArgs e)
         {
             listBoxResults.Items.Clear(); //clean listbox with results
-            string word = this.EnterWordBox.Text.Trim(); //word for search
+            string word = EnterWordBox.Text.Trim(); //word for search
             if(!string.IsNullOrWhiteSpace(word) && list.Count!=0)
             {
-
                 Stopwatch timer = new Stopwatch();
                 timer.Start();
+                listBoxResults.BeginUpdate();
                 foreach (string str in list)
                 {
                     
                     if (str.ToUpper().Contains(word.ToUpper()))
                     {
-                        listBoxResults.Items.Add(str);                                             
-                    }
+                        listBoxResults.Items.Add(str);                                         
+                    }                
                 }
+                listBoxResults.EndUpdate();  
                 timer.Stop();
-                this.Timer2Box.Text = timer.Elapsed.ToString();
-                
+                Timer2Box.Text = timer.Elapsed.ToString();                
             }
             else MessageBox.Show("Choose file and enter word."); 
         }
-
-
     }
 }
